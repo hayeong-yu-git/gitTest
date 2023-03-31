42479b7407c9573028645eff6d842f702b05f139
hayeong-yu-git <seongtan2020@gmail.com>
2023-04-01 02:26:53 +0900

    delete 4 line.

--- src/com/yhy/test/TestClass.java
+++ src/com/yhy/test/TestClass.java
@@ -4,10 +4,7 @@ public class TestClass {
 	
 	public static void main(String[] args) {
 		System.out.println("나는 마스터다");
-		System.out.println("나는 마스터다2");
-		System.out.println("나는 마스터다3");
-		System.out.println("나는 마스터다4");
-		System.out.println("gitTest2");
+		//4줄 삭제
 		
 	}
 
